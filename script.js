4ec4ae69810248aa11b6c3a77d21c761bd0f9ce5
Your Name <you@example.com>
2022-05-22 14:39:49 -0400

    Sasage yo challenged added

--- script.js
+++ script.js
@@ -5,20 +5,35 @@ form.addEventListener("submit", function (e) {
 });
 
 
+let select = document.querySelector("#daForm");
 
-let massText = (iter, targ, txt) => {
-    for(let i = iter; i > 0; i--) {
-        let select = document.querySelector(targ)
-        let div = document.createElement('div')
-        div.classList.add("col-xs-12" , "col-md-6", "col-xl-2" ,"py-2", "px-0", "text-center")
-        let p = document.createElement("p")
-        select.append(div);
-        div.append(p);
-        p.append(txt);
+select.addEventListener("submit", function() {
+    let inputIter = () => {
+        let iterData = document.querySelector("#iterData").value;    
+        return iterData;
     }
-}
 
-massText(6, "#datA", "捧げよ！ 捧げよ！");
+    let massText = (iter) => {
+        for(let i = iter; i > 0; i--) {
+            let select = document.querySelector("#datA")
+            let div = document.createElement('div')
+            div.classList.add("col-xs-12" , "col-md-6", "col-xl-2" ,"py-2", "px-0", "text-center")
+            let p = document.createElement("p")
+            let h2 = document.createElement("h2")
+            select.append(div)
+            div.append(h2)
+            h2.append("PROVE IT!!!")
+            div.append(p)
+            p.append("捧げよ！ 捧げよ！");
+        }
+    }
+    
+    massText(inputIter());
+});
+
+
+
+
 
 
 
