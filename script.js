e4f33277306d7a39ed14c8263f38cf21c4b39b9c
Your Name <you@example.com>
2022-05-22 02:45:38 -0400

    New features and form added, still working on the form.

---
+++ script.js
@@ -0,0 +1,14 @@
+let massText = (iter, targ, txt) => {
+    for(let i = iter; i > 0; i--) {
+        let select = document.querySelector(targ)
+        let div = document.createElement('div')
+        div.classList.add("col-xs-12" , "col-md-6", "col-xl-2" ,"py-2", "px-0", "text-center")
+        let p = document.createElement("p")
+        select.append(div);
+        div.append(p);
+        p.append(txt);
+    }
+}
+
+massText(6, "#datA", "捧げよ！ 捧げよ！");
+
